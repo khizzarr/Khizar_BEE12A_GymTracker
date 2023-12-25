b70e713f268840f56b4a760249c7928b0e91e6ad
Umer Amer Khan <64536327+UmerAmer@users.noreply.github.com>
2023-12-25 21:47:39 +0500

    Update CV_PROJECT.py

--- CV_PROJECT.py
+++ CV_PROJECT.py
@@ -69,6 +69,9 @@ def calculate_angle(a,b,c):
     a = np.array(a) # First
     b = np.array(b) # Mid
     c = np.array(c) # End
+    print(a)
+    print(b)
+    print(c)
     
     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
     angle = np.abs(radians*180.0/np.pi)
